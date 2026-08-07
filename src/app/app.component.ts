cdb1e56590872150c976eb8a7ff9f46dfb71f1aa
alexschoefer <alex.schoefer@gmx.net>
2026-08-07 19:54:59 +0200

    feat: remove imports app.components.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './layout/header/header.component'; // Ensure this path is correct
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, Header], // Ensure Header is a standalone component or part of a module
+  imports: [CommonModule, RouterOutlet], // Ensure Header is a standalone component or part of a module
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
