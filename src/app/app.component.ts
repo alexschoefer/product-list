52123483d3cc3a7f38d80933afe3ef96f02c55f7
alexschoefer <alex.schoefer@gmx.net>
2026-08-07 19:48:41 +0200

    project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header.component'; // Ensure this path is correct
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, Header], // Ensure Header is a standalone component or part of a module
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
