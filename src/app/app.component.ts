6f8804d1d13e3d2fa61ee9acdb179795a3f79acd
alexschoefer <alex.schoefer@gmx.net>
2026-08-07 20:22:52 +0200

    feat: add first styles + html structure of product-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-
+import { HeaderComponent } from './layout/header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { ProductListComponent } from "./shared/components/product-list/product-list.component";
 
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet], // Ensure Header is a standalone component or part of a module
+    imports: [RouterOutlet, HeaderComponent, ProductListComponent], 
     templateUrl: './app.component.html',
     changeDetection: ChangeDetectionStrategy.Eager,
     styleUrl: './app.component.scss'
